71f99fa6b648f7f6114289504349b0fdb415c6b3
jiaheng.zhang <jiaheng.zhang@rea-group.com>
2020-06-13 18:54:22 +0800

    Task2: Should throw exception when store given locker is full.

--- src/main/java/com/tw/Locker.java
+++ src/main/java/com/tw/Locker.java
@@ -7,6 +7,10 @@ public class Locker {
     private int capacity;
 
     public Ticket store(Bag bag) {
+        if (capacity==0) {
+            throw new RuntimeException("Locker has already full");
+        }
+        capacity--;
         return Ticket.builder().bagId(bag.getId()).build();
     }
 }
--- src/test/java/com/tw/LockerTest.java
+++ src/test/java/com/tw/LockerTest.java
@@ -3,10 +3,11 @@ package com.tw;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LockerTest {
     @Test
-    public void shouldStoreBagSuccessGivenEmptyLockerThenReturnATicket() {
+    public void shouldStoreBagSuccessAndReturnATicketWhenStoreGivenEmptyLocker() {
         var locker = Locker.builder().capacity(3).build();
         var bagId = 1;
         var bag = Bag.builder().id(bagId).build();
@@ -18,7 +19,7 @@ public class LockerTest {
     }
 
     @Test
-    public void shouldStoreBagSuccessGivenLockerJustHave1SpaceThenReturnATicket() {
+    public void shouldStoreBagSuccessAndReturnATicketWhenStoreGivenLockerJustHave1Space() {
         var locker = Locker.builder().capacity(3).build();
         var bag1Id = 1;
         var bag2Id = 2;
@@ -33,5 +34,21 @@ public class LockerTest {
 
         var expectedTicket = Ticket.builder().bagId(bag3Id).build();
         assertThat(ticket).isEqualTo(expectedTicket);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenStoreGivenLockerWithoutSpace() {
+        var locker = Locker.builder().capacity(2).build();
+        var bag1Id = 1;
+        var bag2Id = 2;
+        var bag3Id = 3;
+        var bag1 = Bag.builder().id(bag1Id).build();
+        var bag2 = Bag.builder().id(bag2Id).build();
+        var bag3 = Bag.builder().id(bag3Id).build();
+
+        locker.store(bag1);
+        locker.store(bag2);
+
+        assertThatThrownBy(()->locker.store(bag3)).isInstanceOf(RuntimeException.class);
     }
 }
